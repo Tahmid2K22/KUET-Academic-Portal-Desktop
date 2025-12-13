4ccd6e2471b6e7920c21d0a779301c632de4fe5c
TAHMID2K22 <chowdhurytahmid20@gmail.com>
2025-12-13 11:28:51 +0600

    Initialize KUET Academic Portal Desktop project with JavaFX and Maven setup

---
+++ src/main/java/com/example/kuet_academic_portal_desktop/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.kuet_academic_portal_desktop;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.kuet_academic_portal_desktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.kuet_academic_portal_desktop to javafx.fxml;
+    exports com.example.kuet_academic_portal_desktop;
+}
